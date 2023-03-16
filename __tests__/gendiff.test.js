a0500e4b79f68aa59a8e0e7066b179322c0e0e20
vladislav_shalimov <shalimov-vlad132@mail.ru>
2023-03-16 13:23:23 +0400

    add gendiff.test.js

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,23 @@
+import genDiff from '../src/gendiff.js';
+import { test, expect } from '@jest/globals';
+
+test('flat json comparisons', () => {
+  expect(genDiff('/home/vladislav_shalimov/frontend-project-46/__fixtures__/file1.json', '/home/vladislav_shalimov/frontend-project-46/__fixtures__/file2.json')).toEqual(
+`{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`);
+  expect(genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json')).toEqual(
+`{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`);
+});
