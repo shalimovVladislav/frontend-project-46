690890b3e239da8ed63ae602f2b24123df958f47
vladislav_shalimov <shalimov-vlad132@mail.ru>
2023-03-16 14:38:01 +0400

    fix gendiff.test.js

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,7 +2,7 @@ import { test, expect } from '@jest/globals';
 import genDiff from '../src/gendiff.js';
 
 test('flat json comparisons', () => {
-  expect(genDiff('./__fixtures__/file1.json', './frontend-project-46/__fixtures__/file2.json')).toEqual(
+  expect(genDiff('./__fixtures__/file1.json', './/__fixtures__/file2.json')).toEqual(
     `{
   - follow: false
     host: hexlet.io
