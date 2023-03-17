210317485f8c8bf6f2fe3314af2b09bf70a9c7b4
vladislav_shalimov <shalimov-vlad132@mail.ru>
2023-03-17 21:08:56 +0400

    fix gendiff.js

--- src/gendiff.js
+++ src/gendiff.js
@@ -6,6 +6,25 @@ const genDiff = (filepath1, filepath2) => {
   const obj2 = filePathtoObject(filepath2);
   const spacesCount = 2;
   const replacer = ' ';
+
+  const iter1 = (node, depth) => {
+    if (!_.isObject(node)) {
+      return `${node}`;
+    }
+    const indentSize = depth * spacesCount;
+    const currentIndent = replacer.repeat(indentSize);
+    const bracketIndent = replacer.repeat(indentSize - spacesCount);
+    console.log(node);
+    const lines = Object
+      .entries(node)
+      .map(([key, val]) => `${currentIndent}  ${key}: ${iter1(val, depth + 1)}`);
+    return [
+      '{',
+      ...lines,
+      `${bracketIndent}  }`,
+    ].join('\n');
+  };
+
   const iter = (node1, node2, depth) => {
     const keys = _.uniq([...Object.keys(node1), ...Object.keys(node2)].sort());
 
@@ -15,23 +34,21 @@ const genDiff = (filepath1, filepath2) => {
 
     const lines = keys.reduce((acc, key) => {
       if (_.isObject(node1[key]) && _.isObject(node2[key])) {
-        return [...acc, `${currentIndent}${currentIndent}${key}: ${iter(node1[key], node2[key], depth + 2)}`];
+        return [...acc, `${currentIndent}  ${key}: ${iter(node1[key], node2[key], depth + 1)}`];
       }
-
-      /* if (_.isObject(node1[key])) {
-
+      if (_.isObject(node1[key])) {
+        acc.push(`${currentIndent}- ${key}: ${iter1(node1[key], depth + 1)}`);
       }
       if (_.isObject(node2[key])) {
-
-      } */
-
+        acc.push(`${currentIndent}+ ${key}: ${iter1(node2[key], depth + 1)}`);
+      }
       if (node1[key] === node2[key]) {
         return [...acc, `${currentIndent}  ${key}: ${node1[key]}`];
       }
-      if (_.has(node1, key)) {
+      if (_.has(node1, key) && !_.isObject(node1[key])) {
         acc.push(`${currentIndent}- ${key}: ${node1[key]}`);
       }
-      if (_.has(node2, key)) {
+      if (_.has(node2, key) && !_.isObject(node2[key])) {
         acc.push(`${currentIndent}+ ${key}: ${node2[key]}`);
       }
       return acc;
@@ -40,7 +57,7 @@ const genDiff = (filepath1, filepath2) => {
     return [
       '{',
       ...lines,
-      `${bracketIndent}}`,
+      `${bracketIndent}${depth === 1 ? '' : '  '}}`,
     ].join('\n');
   };
   return iter(obj1, obj2, 1);
