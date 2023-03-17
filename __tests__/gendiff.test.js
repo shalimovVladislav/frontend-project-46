5bf199fbb9282de4493deb19048dcedd3b14512f
vladislav_shalimov <shalimov-vlad132@mail.ru>
2023-03-17 14:02:49 +0400

    add test for .yml files

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -13,3 +13,16 @@ test('flat json comparisons', () => {
 }`,
   );
 });
+
+test('flat yaml comparisons', () => {
+  expect(genDiff('./__fixtures__/file1.yml', './/__fixtures__/file2.yml')).toEqual(
+    `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`,
+  );
+});
