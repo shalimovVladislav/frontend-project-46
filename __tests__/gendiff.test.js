6e220022e01f138c1fcd836ac841d3d058e76074
vladislav_shalimov <shalimov-vlad132@mail.ru>
2023-03-17 21:08:21 +0400

    add nested json and yml tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,7 +2,7 @@ import { test, expect } from '@jest/globals';
 import genDiff from '../src/gendiff.js';
 
 test('flat json comparisons', () => {
-  expect(genDiff('./__fixtures__/file1.json', './/__fixtures__/file2.json')).toEqual(
+  expect(genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json')).toEqual(
     `{
   - follow: false
     host: hexlet.io
@@ -15,7 +15,7 @@ test('flat json comparisons', () => {
 });
 
 test('flat yaml comparisons', () => {
-  expect(genDiff('./__fixtures__/file1.yml', './/__fixtures__/file2.yml')).toEqual(
+  expect(genDiff('./__fixtures__/file1.yml', './__fixtures__/file2.yml')).toEqual(
     `{
   - follow: false
     host: hexlet.io
@@ -23,6 +23,104 @@ test('flat yaml comparisons', () => {
   - timeout: 50
   + timeout: 20
   + verbose: true
+}`,
+  );
+});
+
+test('nested json comparisons', () => {
+  expect(genDiff('./__fixtures__/file3.json', './__fixtures__/file4.json')).toEqual(
+    `{
+    common: {
+    + follow: false
+      setting1: Value 1
+    - setting2: 200
+    - setting3: true
+    + setting3: null
+    + setting4: blah blah
+    + setting5: {
+        key5: value5
+      }
+      setting6: {
+        doge: {
+        - wow: 
+        + wow: so much
+        }
+        key: value
+      + ops: vops
+      }
+    }
+    group1: {
+    - baz: bas
+    + baz: bars
+      foo: bar
+    - nest: {
+        key: value
+      }
+    + nest: str
+    }
+  - group2: {
+      abc: 12345
+      deep: {
+        id: 45
+      }
+    }
+  + group3: {
+      deep: {
+        id: {
+          number: 45
+        }
+      }
+      fee: 100500
+    }
+}`,
+  );
+});
+
+test('nested yml comparisons', () => {
+  expect(genDiff('./__fixtures__/file3.yml', './__fixtures__/file4.yml')).toEqual(
+    `{
+    common: {
+    + follow: false
+      setting1: Value 1
+    - setting2: 200
+    - setting3: true
+    + setting3: null
+    + setting4: blah blah
+    + setting5: {
+        key5: value5
+      }
+      setting6: {
+        doge: {
+        - wow: 
+        + wow: so much
+        }
+        key: value
+      + ops: vops
+      }
+    }
+    group1: {
+    - baz: bas
+    + baz: bars
+      foo: bar
+    - nest: {
+        key: value
+      }
+    + nest: str
+    }
+  - group2: {
+      abc: 12345
+      deep: {
+        id: 45
+      }
+    }
+  + group3: {
+      deep: {
+        id: {
+          number: 45
+        }
+      }
+      fee: 100500
+    }
 }`,
   );
 });
