5c930e5605117b94e3ff04c56b3a47fe12d060f6
vladislav_shalimov <shalimov-vlad132@mail.ru>
2023-03-18 21:37:31 +0400

    fix gendiff.js

--- src/gendiff.js
+++ src/gendiff.js
@@ -2,10 +2,22 @@ import filePathtoObject from './parsers.js';
 import createTree from './createTree.js';
 import stylish from './stylish.js';
 
-const genDiff = (filepath1, filepath2) => {
+const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const obj1 = filePathtoObject(filepath1);
   const obj2 = filePathtoObject(filepath2);
   const tree = createTree(obj1, obj2);
-  return stylish(tree);
+  let result;
+  switch (format) {
+    case 'stylish':
+      result = stylish(tree);
+      break;
+    case 'plain':
+      break;
+    case 'json':
+      break;
+    default:
+      throw new Error('genDiff switch exception.');
+  }
+  return result;
 };
 export default genDiff;
