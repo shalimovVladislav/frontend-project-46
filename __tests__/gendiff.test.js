df79acde09cae4401cd0b9a21b1856dc5ee4652f
vladislav_shalimov <shalimov-vlad132@mail.ru>
2023-03-19 16:42:17 +0400

    add nested json, yml tests in json format

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -156,3 +156,15 @@ Property 'group2' was removed
 Property 'group3' was added with value: [complex value]`,
   );
 });
+
+test('nested json comparisons in json format', () => {
+  expect(genDiff('./__fixtures__/file3.json', './__fixtures__/file4.json', 'json')).toEqual(
+    '[{"type":"nested","key":"common","children":[{"type":"add","key":"follow","val":false},{"type":"equal","key":"setting1","val":"Value 1"},{"type":"remove","key":"setting2","val":200},{"type":"not equal","key":"setting3","val":[true,null]},{"type":"add","key":"setting4","val":"blah blah"},{"type":"add","key":"setting5","val":{"key5":"value5"}},{"type":"nested","key":"setting6","children":[{"type":"nested","key":"doge","children":[{"type":"not equal","key":"wow","val":["","so much"]}]},{"type":"equal","key":"key","val":"value"},{"type":"add","key":"ops","val":"vops"}]}]},{"type":"nested","key":"group1","children":[{"type":"not equal","key":"baz","val":["bas","bars"]},{"type":"equal","key":"foo","val":"bar"},{"type":"not equal","key":"nest","val":[{"key":"value"},"str"]}]},{"type":"remove","key":"group2","val":{"abc":12345,"deep":{"id":45}}},{"type":"add","key":"group3","val":{"deep":{"id":{"number":45}},"fee":100500}}]',
+  );
+});
+
+test('nested yml comparisons in json format', () => {
+  expect(genDiff('./__fixtures__/file3.yml', './__fixtures__/file4.yml', 'json')).toEqual(
+    '[{"type":"nested","key":"common","children":[{"type":"add","key":"follow","val":false},{"type":"equal","key":"setting1","val":"Value 1"},{"type":"remove","key":"setting2","val":200},{"type":"not equal","key":"setting3","val":[true,null]},{"type":"add","key":"setting4","val":"blah blah"},{"type":"add","key":"setting5","val":{"key5":"value5"}},{"type":"nested","key":"setting6","children":[{"type":"nested","key":"doge","children":[{"type":"not equal","key":"wow","val":["","so much"]}]},{"type":"equal","key":"key","val":"value"},{"type":"add","key":"ops","val":"vops"}]}]},{"type":"nested","key":"group1","children":[{"type":"not equal","key":"baz","val":["bas","bars"]},{"type":"equal","key":"foo","val":"bar"},{"type":"not equal","key":"nest","val":[{"key":"value"},"str"]}]},{"type":"remove","key":"group2","val":{"abc":12345,"deep":{"id":45}}},{"type":"add","key":"group3","val":{"deep":{"id":{"number":45}},"fee":100500}}]',
+  );
+});
