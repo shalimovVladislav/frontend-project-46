1e5113a828e5513371bafd1f68eb2553d8b1f62e
vladislav_shalimov <shalimov-vlad132@mail.ru>
2023-03-19 19:25:24 +0400

    gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { Command, Option } from 'commander';
-import genDiff from '../src/gendiff.js';
+import genDiff from '../index.js';
 
 const program = new Command();
 program
