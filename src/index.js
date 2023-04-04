0fc1a485b3f9f239999749721feab2b1fb315354
vladislav_shalimov <shalimov-vlad132@mail.ru>
2023-04-04 15:46:18 +0400

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import * as fs from 'node:fs';
 import path from 'node:path';
 import { cwd } from 'node:process';
-import parsers from './parsers.js';
+import parseData from './parsers.js';
 import createTree from './createTree.js';
 import formatter from './formatters/index.js';
 
@@ -12,8 +12,8 @@ const genDiff = (filePath1, filePath2, format) => {
   const fullFilePath2 = path.resolve(cwd(), filePath2);
   const file1 = fs.readFileSync(fullFilePath1, 'utf-8');
   const file2 = fs.readFileSync(fullFilePath2, 'utf-8');
-  const obj1 = parsers(file1, fileExtension1);
-  const obj2 = parsers(file2, fileExtension2);
+  const obj1 = parseData(file1, fileExtension1);
+  const obj2 = parseData(file2, fileExtension2);
   const tree = createTree(obj1, obj2);
   const result = formatter(tree, format);
   return result;
