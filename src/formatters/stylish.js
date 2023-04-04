8ab2d5b73cbfadda32395559839673ed7fee2700
vladislav_shalimov <shalimov-vlad132@mail.ru>
2023-04-04 21:29:23 +0400

    fix stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -24,38 +24,39 @@ const stringify = (initialNode, initialDepth) => {
   return iter(initialNode, initialDepth);
 };
 
-const getIndentSize = (depth) => depth * spacesCount;
+const getIndent = (depth) => {
+  const indentSize = depth * spacesCount;
+  return replacer.repeat(indentSize - 2);
+};
 
-const getCurrentIndent = (indentSize) => replacer.repeat(indentSize - 2);
-
-const getBracketIndent = (indentSize) => replacer.repeat(indentSize - spacesCount);
+const getBracketIndent = (depth) => {
+  const indentSize = depth * spacesCount;
+  return replacer.repeat(indentSize - spacesCount);
+};
 
 const stylish = (nodesArray) => {
   const iter = (nodes, depth) => {
-    const indentSize = getIndentSize(depth);
-    const currentIndent = getCurrentIndent(indentSize);
-    const bracketIndent = getBracketIndent(indentSize);
+    const indent = getIndent(depth);
     const lines = nodes.flatMap((node) => {
       switch (node.type) {
         case 'nested':
-          return `${currentIndent}  ${node.key}: ${iter(node.children, depth + 1)}`;
+          return `${indent}  ${node.key}: ${iter(node.children, depth + 1)}`;
         case 'removed':
-          return `${currentIndent}- ${node.key}: ${stringify(node.value, depth + 1, replacer, spacesCount)}`;
+          return `${indent}- ${node.key}: ${stringify(node.value, depth + 1, replacer, spacesCount)}`;
         case 'added':
-          return `${currentIndent}+ ${node.key}: ${stringify(node.value, depth + 1, replacer, spacesCount)}`;
+          return `${indent}+ ${node.key}: ${stringify(node.value, depth + 1, replacer, spacesCount)}`;
         case 'equal':
-          return `${currentIndent}  ${node.key}: ${node.value}`;
+          return `${indent}  ${node.key}: ${node.value}`;
         case 'modified':
           return [
-            `${currentIndent}- ${node.key}: ${_.isObject(node.oldValue) ? stringify(node.oldValue, depth + 1) : node.oldValue}`,
-            `${currentIndent}+ ${node.key}: ${_.isObject(node.newValue) ? stringify(node.newValue, depth + 1) : node.newValue}`,
+            `${indent}- ${node.key}: ${_.isObject(node.oldValue) ? stringify(node.oldValue, depth + 1) : node.oldValue}`,
+            `${indent}+ ${node.key}: ${_.isObject(node.newValue) ? stringify(node.newValue, depth + 1) : node.newValue}`,
           ];
         default:
           throw new Error(`Unknown type of data: ${node.type}`);
       }
     });
-
-    return ['{', ...lines, `${bracketIndent}}`].join('\n');
+    return ['{', ...lines, `${getBracketIndent(depth)}}`].join('\n');
   };
 
   return iter(nodesArray, 1);
